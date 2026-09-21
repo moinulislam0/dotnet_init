c4fea4483e4688fcb06ff6dd8722c3d0326651c9
Md.Moinul Islam <moinul4838@gmail.com>
2026-09-21 23:39:29 +0600

    covert

--- Program.cs
+++ Program.cs
@@ -3,15 +3,24 @@
 class Test {
 
     public static void Main(){
-    string name = " Md Moinul Islam";
-    int age = 25;
-    double number = 12.3;
-    bool isTrue = false;
-    char bloodgroup = 'A';
-    Console.WriteLine(name);
-    Console.WriteLine(age);
-    Console.WriteLine(number);
-    Console.WriteLine(isTrue);
-    Console.WriteLine(bloodgroup);
+    // string name = " Md Moinul Islam";
+    // int age = 25;
+    // double number = 12.3;
+    // bool isTrue = false;
+    // char bloodgroup = 'A';
+    // Console.WriteLine($"Name : {name}");
+    // Console.WriteLine(age);
+    // Console.WriteLine(number);
+    // Console.WriteLine(isTrue);
+    // Console.WriteLine(bloodgroup);
+    double salary = 2458.2;
+    Console.WriteLine(salary);
+
+    int salary2 = (int)salary;
+    Console.WriteLine(salary2);
+
+    Console.WriteLine(Convert.ToString(salary2));
+    Console.WriteLine(Convert.ToDouble(salary2));
+
     }
 }
