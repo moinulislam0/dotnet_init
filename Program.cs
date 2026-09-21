d5c0d4e05e1c119977f2b0ce15e84d2747965c51
Md.Moinul Islam <moinul4838@gmail.com>
2026-09-21 23:48:33 +0600

    convert

--- Program.cs
+++ Program.cs
@@ -13,14 +13,19 @@ class Test {
     // Console.WriteLine(number);
     // Console.WriteLine(isTrue);
     // Console.WriteLine(bloodgroup);
-    double salary = 2458.2;
-    Console.WriteLine(salary);
+    // double salary = 2458.2;
+    // Console.WriteLine(salary);
 
-    int salary2 = (int)salary;
-    Console.WriteLine(salary2);
+    // int salary2 = (int)salary;
+    // Console.WriteLine(salary2);
 
-    Console.WriteLine(Convert.ToString(salary2));
-    Console.WriteLine(Convert.ToDouble(salary2));
-
+    // Console.WriteLine(Convert.ToString(salary2));
+    // Console.WriteLine(Convert.ToDouble(salary2));
+    // Console.WriteLine(Convert.ToInt32(salary));
+    String input = "15";
+    int result;
+    bool isSuccess = double.TryParse(input ,out result);
+    Console.WriteLine(result);
+    Console.WriteLine(isSuccess);
     }
 }
